1668f1cadfc4b5621444815ad4cd6380baa238a8
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-09-27 11:22:27 -0400

    update: user squema and seeding

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -35,7 +35,7 @@ export const login = async (req: Request, res: Response) => {
 export const signUp = async (req: Request, res: Response) => {
   try {
     const { username, email, password } = req.body;
-    const newUser = await User.create({ username, email, password });
+    const newUser = await User.create({ username, email, password, role: 'user' });
 
     console.log(newUser);
     
